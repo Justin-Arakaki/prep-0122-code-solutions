2323fed9977ff88a7022c0e0526c891930fed976
Justin-Arakaki <Justiniankoi@gmail.com>
2022-01-18 15:12:15 -0800

    Reordered console.log so that it appears correctly in Console

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -4,11 +4,12 @@ const person = {
   hobby: 'Carpentry'
 };
 
+console.log(person);
+console.log('The person\'s name is:', person.firstName);
+
 person.job = 'General Contractor';
 person.previousJob = 'Career Technopath';
 
-console.log(person);
-console.log('The person\'s name is:', person.firstName);
 console.log('The person\'s current job is:', person.job);
 console.log('The person\'s previous job is:', person.previousJob);
 console.log('The compete person object:', person);
