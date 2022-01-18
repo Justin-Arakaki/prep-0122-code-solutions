3cb947930a965007bd1e23dd7a15f6e7e30271e0
Justin-Arakaki <Justiniankoi@gmail.com>
2022-01-18 15:07:06 -0800

    Completed exercise

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,14 @@
+const person = {
+  firstName: 'Bob',
+  lastName: 'Builder',
+  hobby: 'Carpentry'
+};
+
+person.job = 'General Contractor';
+person.previousJob = 'Career Technopath';
+
+console.log(person);
+console.log('The person\'s name is:', person.firstName);
+console.log('The person\'s current job is:', person.job);
+console.log('The person\'s previous job is:', person.previousJob);
+console.log('The compete person object:', person);
